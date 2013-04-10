dde53ef0e6cf5d895543906fa106ca2c80eba971
didier.cherix <didier.cherix@localhost>
2013-04-10 20:27:15 +0200

    ...

--- src/main/java/jdomaincrawler/controller/ThreadExecutor.java
+++ src/main/java/jdomaincrawler/controller/ThreadExecutor.java
@@ -57,7 +57,6 @@ public class ThreadExecutor {
 		queue = new PriorityBlockingQueue<Runnable>(
 				Integer.valueOf(PropertiesFactory.getProperties().getProperty(
 						"queueLength", "11")), comp);
-		System.out.println(poolSize);
 		threadPool = new ThreadPoolExecutor(poolSize, maxPoolSize, timeout,
 				TimeUnit.SECONDS, queue);
 	}
